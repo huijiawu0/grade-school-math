11c070b25244e536d2bde4e240e563786504b596
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-10-08 17:23:01 +0800

    add func

--- grade_school_math/dataset.py
+++ grade_school_math/dataset.py
@@ -72,4 +72,4 @@ class GSMDataset(th.utils.data.Dataset):
         )
         tokens = th.tensor(tokens)
         mask = th.tensor(mask)
-        return dict(input_ids=tokens, attention_mask=mask)
+        return dict(input_ids=tokens, attention_mask=mask, labels=tokens)
