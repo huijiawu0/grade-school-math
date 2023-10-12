4a76f962cc566ad2b732ac012457910fc6d3935e
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-10-12 15:37:03 +0800

    add func

--- grade_school_math/batch_sampler.py
+++ grade_school_math/batch_sampler.py
@@ -99,6 +99,7 @@ def main():
     ray.init()
     # Divide the eval_loader into chunks based on the number of GPUs
     num_gpus = torch.cuda.device_count()
+    print("num_gpus: ", num_gpus)
     eval_loader_chunks = list(eval_loader)
     eval_loader_chunks = [eval_loader_chunks[i::num_gpus] for i in range(num_gpus)]
 
