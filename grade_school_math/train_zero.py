cf3c75415832053650b01e0f230c216e4a77de60
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-12-15 10:12:50 +0800

    1

--- grade_school_math/train_zero.py
+++ grade_school_math/train_zero.py
@@ -148,7 +148,6 @@ def main():
     for epoch in range(num_epochs):
         for batch in train_loader:
             optim.zero_grad()
-            print(batch)
             batch = {k: v.to(device) for k, v in batch.items()}
             outputs = model(**batch, labels=batch["input_ids"])
             loss = outputs[0]
