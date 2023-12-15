2664c7d1cb2ee0671ba69efefdf93610265f93b3
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-12-15 10:10:11 +0800

    1

--- grade_school_math/train_zero.py
+++ grade_school_math/train_zero.py
@@ -1,19 +1,53 @@
+import os
 import re
 
 import torch
 from transformers.trainer_pt_utils import LabelSmoother
 
-from dataset import get_examples, GSMDataset
+from dataset import read_jsonl
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, GenerationConfig
 from transformers import GPT2Config, AdamW
 from transformers import get_scheduler
 from tqdm.auto import tqdm
 from torch.utils.data import DataLoader
 
-
 IGNORE_TOKEN_ID = LabelSmoother.ignore_index
 ANS_RE = re.compile(r"#### (\-?[0-9\.\,]+)")
 INVALID_ANS = "[invalid]"
+
+
+class GSMDataset(torch.utils.data.Dataset):
+    def __init__(self, tokenizer, examples, loss_on_prefix=True):
+        self.examples = examples
+        self.qns = [ex["question"] for ex in self.examples]
+        self.ans = [ex["answer"] for ex in self.examples]
+        self.qns = tokenizer(self.qns, padding=False)
+        self.ans = tokenizer(self.ans, padding=False)
+        self.loss_on_prefix = loss_on_prefix
+        self.max_len = max(
+            [
+                len(self.qns["input_ids"][i]) + len(self.ans["input_ids"][i])
+                for i in range(len(self.examples))
+            ]
+        )
+        print(f"Max tokens: {self.max_len}")
+    
+    def __len__(self):
+        return len(self.examples)
+    
+    def __getitem__(self, idx):
+        qn_tokens = self.qns["input_ids"][idx]
+        ans_tokens = self.ans["input_ids"][idx]
+        pad_tokens = [0] * (self.max_len - len(qn_tokens) - len(ans_tokens))
+        tokens = qn_tokens + ans_tokens + pad_tokens
+        mask = (
+                ([int(self.loss_on_prefix)] * len(qn_tokens))
+                + ([1] * len(ans_tokens))
+                + ([0] * len(pad_tokens))
+        )
+        tokens = torch.tensor(tokens)
+        mask = torch.tensor(mask)
+        return dict(input_ids=tokens, attention_mask=mask)
 
 
 def extract_answer(completion):
@@ -72,6 +106,18 @@ def eval(model, eval_dataloader, tokenizer):
     print(len(rg), invalid)
 
 
+def get_examples(split):
+    path = os.path.join("data/", f"{split}.jsonl")
+    examples = read_jsonl(path)
+    
+    for ex in examples:
+        ex.update(question=ex["question"] + "\n")
+        ex.update(answer=ex["answer"] + "<|endoftext|>")
+    
+    print(f"{len(examples)} {split} examples")
+    return examples
+
+
 def main():
     tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
     tokenizer.pad_token = tokenizer.unk_token
@@ -97,13 +143,13 @@ def main():
         num_warmup_steps=0,
         num_training_steps=num_training_steps,
     )
-
+    
     pbar = tqdm(range(num_training_steps))
     for epoch in range(num_epochs):
         for batch in train_loader:
             optim.zero_grad()
             print(batch)
-            batch = {k: v.to(device) for k, v in batch.items()}
+            # batch = {k: v.to(device) for k, v in batch.items()}
             outputs = model(**batch, labels=batch["input_ids"])
             loss = outputs[0]
             loss.backward()
